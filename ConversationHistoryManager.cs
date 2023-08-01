140801cace61e28cb06e6e78ead5c58b12b4290e
drojasmcr <diego@rojascr.net>
2023-08-01 10:22:16 -0600

    Adding ConversationHistory method consumption

---
+++ ConversationHistoryManager.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Alterna
+{
+    public class ConversationHistoryManager
+    {
+        public List<ConversationHistory> GetConversations(string state, DateTime startDate, DateTime endDate)
+        {
+            return null;
+        }
+    }
+}
